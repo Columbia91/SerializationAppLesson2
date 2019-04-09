4c0dbc5a319f4ede2bd8586a02706712aefdd031
Columbia91 <alpha.z91@mail.ru>
2019-04-09 19:41:13 +0600

    Добавьте файлы проекта.

---
+++ SerializationAppLesson/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationAppLesson
+{
+    [Serializable]
+    class User
+    {
+        [NonSerialized]
+        private int id;
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
